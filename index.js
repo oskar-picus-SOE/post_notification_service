7d9cb2caa2cdeb4e7f74c0db72bed4f8b3a0d0d3
Oskar Picus <oskarpicus@yahoo.com>
2023-12-16 17:03:29 +0200

    Added Kafka consumer

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const kafka = require('kafka-node'),
+    Consumer = kafka.Consumer,
+    client = new kafka.KafkaClient(),
+    consumer = new Consumer(
+        client,
+        [
+            {topic: 'postNotifications', partition: 0},
+        ],
+        {
+            autoCommit: false
+        }
+    );
+
+consumer.on('message', function (message) {
+    const post = JSON.parse(message.value)
+    console.log(post)
+    // todo send notification with web socket
+});
