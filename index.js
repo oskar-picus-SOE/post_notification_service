4f1cbd7549c78af9bc3bae3cb8a10b2dd4fc3bb9
Oskar Picus <oskarpicus@yahoo.com>
2023-12-16 18:55:55 +0200

    Added websocket

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+const { WebSocket, WebSocketServer } = require('ws');
+const http = require('http');
+const uuidv4 = require('uuid').v4;
+
+// Spinning the http server and the WebSocket server.
+const server = http.createServer();
+const wsServer = new WebSocketServer({ server });
+const port = 8004;
+server.listen(port, () => {
+    console.log(`WebSocket server is running on port ${port}`);
+});
+
+// active connections
+const clients = {};
+
+function broadcastMessage(json) {
+    console.log(`Broadcasting message ${json} to all ${Object.keys(clients).length} clients`)
+    // We are sending the current data to all connected clients
+    const data = JSON.stringify(json);
+    for(let userId in clients) {
+        let client = clients[userId];
+        if(client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    }
+}
+
+function handleDisconnect(userId) {
+    console.log(`${userId} disconnected.`);
+    delete clients[userId];
+}
+
+// A new client connection request received
+wsServer.on('connection', function(connection) {
+    // Generate a unique code for every user
+    const userId = uuidv4();
+    console.log('Received a new connection');
+
+    // Store the new connection and handle messages
+    clients[userId] = connection;
+    console.log(`${userId} connected.`);
+    // User disconnected
+    connection.on('close', () => handleDisconnect(userId));
+    // send previous posts
+    postsJson.forEach(postJson => broadcastMessage(postJson));
+});
+
 const kafka = require('kafka-node'),
     Consumer = kafka.Consumer,
     client = new kafka.KafkaClient(),
@@ -11,8 +58,10 @@ const kafka = require('kafka-node'),
         }
     );
 
+const postsJson = []
+
 consumer.on('message', function (message) {
-    const post = JSON.parse(message.value)
-    console.log(post)
-    // todo send notification with web socket
+    console.log(`Consumed message from Kafka ${message.value}`)
+    postsJson.push(message.value)
+    broadcastMessage(message.value)
 });
